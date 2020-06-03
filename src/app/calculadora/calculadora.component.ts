8bd08cd03d9a746b9086f91e18cfd5465c58e569
Michael Vega <micha3lvega@gmail.com>
2020-06-03 17:01:46 -0500

    crear componente calculadora

---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+})
+export class CalculadoraComponent {
+  value1: number = 0;
+  value2: number = 0;
+}
